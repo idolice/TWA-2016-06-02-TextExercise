75409e91bc8f9fc5a56711f79d1352bdf0e15777
idolice <349031587@qq.com>
2016-06-02 14:03:23 +0800

    change

--- checkNumber.js
+++ checkNumber.js
@@ -11,19 +11,9 @@ $(document).ready(function(){
                 document.getElementById("datetext").style.borderColor="red";
                 var a="<div  id=\"result\" style=\"color:red\">wrong form </div>";
                 $("#submitbutton").after(a);
-                return 0;
-                        }
-            var str1=str.replace("/","");
-            var reg1=new RegExp(/^d*$/);
-            var r1=reg1.test(str1);
-            if(r==false){
-                document.getElementById("datetext").style.borderColor="red";
-                var a="<div  id=\"result\" style=\"color:red\">input must be number </div>";
-                $("#submitbutton").after(a);
-                return 0;
-                        }
-            return 1;
+
+            }
         }
-        return 0;
+
     })
  })
